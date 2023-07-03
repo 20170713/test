536cb5a1da6ce0e662064d147d4bfb5fe47846b4
20170713 <15218241180@163.com>
2023-07-03 18:04:05 +0800

    完成了分类页面的开发

--- pages/cate/cate.vue
+++ pages/cate/cate.vue
@@ -1,6 +1,26 @@
 <template>
   <view>
-    Cate
+    <view class="scroll-view-container">
+      <!-- 左侧的滚动区域 -->
+      <scroll-view class="left-scroll-view" scroll-y="true" :style="{height: wh + 'px'}">
+        <block v-for="(item,i) in cateList" :key="i">
+          <view :class="['left-scroll-view-item', i === this.active ? 'active' : '']" @click="activeChanged(i)">{{item.cat_name}}</view>
+        </block>
+        
+      </scroll-view>
+      <scroll-view class="right-scroll-view" scroll-y="true" :style="{height: wh + 'px'}" :scroll-top="scrollTop">
+        <view class="cate-level2" v-for="(item2,i2) in cateLevel2" :key="i2">
+          <view class="cate-level2-title">/ {{item2.cat_name}} /</view>
+          <view class="cate-lv3-list" >
+            <view class="cate-lv3-item" v-for="(item3,i3) in item2.children" :key="i3" @click="gotoGoodList(item3)">
+              <image :src="item3.cat_icon"></image>
+              <text>{{item3.cat_name}}</text>
+            </view>
+          </view>
+        </view>
+        
+      </scroll-view>
+    </view>
   </view>
 </template>
 
@@ -8,12 +28,97 @@
   export default {
     data() {
       return {
-        
+        //可用高度
+        wh: 0,
+        //一级列表的数据
+        cateList: [],
+        //选中
+        active: 0,
+        //二级列表的数据
+        cateLevel2: [],
+        //滚动条距离顶部的距离
+        scrollTop: 0,
       };
+    },
+    onLoad() {
+     const sysInfo = uni.getSystemInfoSync()
+     this.wh = sysInfo.windowHeight
+     this.getCateList()
+    },
+    methods: {
+      async getCateList() {
+        const {data : res} = await uni.$http.get('/api/public/v1/categories')
+        if (res.meta.status !== 200) return uni.$showMsg()
+        this.cateList = res.message
+        this.cateLevel2 = res.message[0].children
+      },
+      activeChanged(i) {
+        this.active = i
+        this.cateLevel2 = this.cateList[i].children
+        this.scrollTop = this.scrollTop === 0 ? 1 : 0
+      },
+      gotoGoodList(item3) {
+        uni.navigateTo({
+          url: '/subpkg/goods_list/goods_list?cid=' + item3.cat_id
+        })
+      }
     }
   }
 </script>
 
 <style lang="scss">
+.scroll-view-container {
+  display: flex;
+  background-color: #fff;
+  .left-scroll-view {
+    width: 120px;
+    .left-scroll-view-item {
+      line-height: 60px;
+      background-color: #f7f7f7;
+      text-align: center;
+      font-size: 12px;
+      position: relative;
+      &.active {
+        background-color: #fff;
+        &::before {
+          content: '';
+          display: block;
+          width: 3px;
+          height: 30px;
+          background-color: #c00000;
+          position: absolute;
+          top: 50%;
+          left: 0;
+          transform: translateY(-50%);
+        }
+      }
+    }
+  }
+}
 
+.cate-level2-title {
+  font-weight: bold;
+  text-align: center;
+  font-size: 12px;
+  padding: 15 px 0;
+}
+
+.cate-lv3-list {
+  display: flex;
+  flex-wrap: wrap;
+  .cate-lv3-item{
+    width: 33.33%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-bottom: 10px;
+    image {
+      width: 60px;
+      height: 60px;
+    }
+    text {
+      font-size: 12px
+    }
+  }
+}
 </style>
