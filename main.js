989b9e25f121f7d97497a0c0c81b4d1563b617ce
20170713 <15218241180@163.com>
2023-07-03 16:23:38 +0800

    完成home首页的开发

--- main.js
+++ main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 
+
 Vue.config.productionTip = false
 
 App.mpType = 'app'
@@ -22,4 +23,34 @@ export function createApp() {
     app
   }
 }
+// #endif
+
+// #ifdef VUE3
+// 导入网络请求的包
+import { $http } from '@escook/request-miniprogram';
+
+uni.$http = $http
+$http.baseUrl = "https://api-hmugo-web.itheima.net"
+
+//请求拦截器
+$http.beforeRequest = function(options) {
+  uni.showLoading({
+    title: "数据加载中..."
+  })
+}
+
+//响应拦截器
+$http.afterRequest = function(options) {
+  uni.hideLoading()
+}
+
+//自定义弹框
+uni.$showMsg = function (title = "数据加载失败！",duration = 1500) {
+  uni.showToast({
+    title,
+    duration,
+    icon: 'none'
+  })
+}
+  
 // #endif
